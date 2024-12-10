a73c7e9e9a49b0117812f8e9fb812fbb875f3df7
Quartararo <barberisvalentin709@gmail.com>
2024-12-10 16:12:01 -0300

    Comenzando con el desarrollo del videojuego.

--- game.js
+++ game.js
@@ -3,19 +3,56 @@ const config = {
   type: Phaser.AUTO,
   width: 256,
   height: 244,
-  backgroundColor: '#fff',
+  backgroundColor: '#049cd8',
   parent: 'game',
   scene: {
-    preload, // se ejecuta para precargar recursos
-    create, // se ejecuta cuando el juego comienza
-    update // se ejecuta en cada frame
+    preload, // se ejecuta para precargar recursos.
+    create, // se ejecuta cuando el juego comienza.
+    update // se ejecuta en cada frame.
   }
 }
 
 new Phaser.Game(config)
+// this -> game -> el juego que estamos construyendo.
 
-function preload () {}
+function preload() {
+  this.load.image(
+    'cloud1',
+    'assets/scenery/overworld/cloud1.png'
+  )
 
-function create () {}
+  this.load.image(
+    'floorbricks',
+    'assets/scenery/overworld/floorbricks.png'
+  )
 
-function update () {}
+  this.load.spritesheet(
+    'mario',
+    'assets/entities/mario.png',
+    { frameWidth: 18, frameHeight: 16 } // <-- dimensiones de cada frame
+  )
+}
+
+function create() {
+  // image(x, y, id-del-asset)
+  this.add.image(100, 50, 'cloud1')
+    .setOrigin(0, 0)
+    .setScale(0.15)
+
+  this.add.tileSprite(0, config.height, config.width, 32, 'floorbricks')
+    .setScale(1)
+    .setOrigin(0, 1)
+
+  this.mario = this.add.sprite(50, 210, 'mario')
+    .setOrigin(0, 1)
+
+  this.keys = this.input.keyboard.createCursorKeys()
+}
+
+function update() {
+  if (this.keys.left.isDown) {
+    this.mario.x -= 2
+  } else if (this.keys.right.isDown) {
+    this.mario.x += 2
+  }
+}
